97b369c842fc0446835947e921ed1ca5e462d703
Ben sinvani <Bensinvani7@gmail.com>
2025-08-14 15:43:49 +0300

    04: cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap"; // import GSAP
 import { ScrollTrigger, SplitText } from "gsap/all"; //import GSAP Plugins
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 // then register them
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktails />
     </main>
   );
 };
